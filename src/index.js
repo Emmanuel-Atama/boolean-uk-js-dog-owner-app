74b6ae851e2a95c37b8a36e7247c14e04b913bfd
Emmanuel Atama <emmanuelatama@outlook.com>
2021-07-26 12:57:39 +0100

    Recent Update

--- src/index.js
+++ src/index.js
@@ -13,84 +13,105 @@ console.log(data);
 
 const dogListEl = document.querySelector(".dogs-list")
 
-function createDogsListItem(dogs) {
+function renderDogsList(dogs) {
   for (let i = 0; i < dogs.length; i++) {
     const dog = dogs[i]
-    // console.log("Inside Dog's Data: ", dogs)
-
     const name = dog.name
+
     const listEl = document.createElement("li")
 
     listEl.className = "dogs-list__button"
 
     listEl.innerText = name
 
+    listEl.addEventListener("click", () => {console.log("OnClick inside renderDogsList: ", dog)
+  
+    renderMainCard(dog)
+  })
+
     dogListEl.append(listEl)
   }
   return listEl
 }
-const CreateDogsList = createDogsListItem(data)
+const CreateDogsList = renderDogsList(data)
 
 
-const SectionEl =  document.querySelector(".main__dog-section");
+const mainSectionEl =  document.querySelector(".main__dog-section")
 
-function createCardElement(dog) {
+function renderMainCard(dog) {
+mainSectionEl.innerHTML = ""
+
+const h2El = document.createElement("h2")
+ h2El.innerText = dog.name
+ mainSectionEl.append = (h2El)
+
+ const imageEl = document.createElement("img")
+ imageEl.setAttribute("height", 300)
+ imageEl.setAttribute("width", 400)
+    const imageSrc = dog.image
+    imageEl.src = imageSrc
+    mainSectionEl.append(imageEl)
+}
+
+
+
+// function createCardElement(dog) {
   
-    const sectionEl = document.querySelector("section");
-    sectionEl.className = "main__dog-section";
-    mainEl.append(sectionEl);
+//     const sectionEl = document.querySelector("section");
+//     sectionEl.className = "main__dog-section";
+//     mainEl.append(sectionEl);
 
-    const h2El = document.querySelector("h2");
-    h2El.innerText = dog.name;
+//     const h2El = document.querySelector("h2");
+//     h2El.innerText = dog.name;
 
-    const imageEl = document.createElement("img");
-    const imageSrc = dog.image;
-    // console.log("Dogs Images: ", image);
-    imageEl.setAttribute("src", imageSrc);
-    sectionEl.append(imageEl);
+//     const imageEl = document.createElement("img");
+//     const imageSrc = dog.image;
+//     // console.log("Dogs Images: ", image);
+//     imageEl.setAttribute("src", imageSrc);
+//     sectionEl.append(imageEl);
 
-    const divEl = document.createElement("div");
-    divEl.className = "main__dog-section__desc";
-    sectionEl.append(divEl);
+//     const divEl = document.createElement("div");
+//     divEl.className = "main__dog-section__desc";
+//     sectionEl.append(divEl);
 
-    const h3El = document.createElement("h3");
-    h3El.innerText = "Bio";
-    divEl.append(h3El);
+//     const h3El = document.createElement("h3");
+//     h3El.innerText = "Bio";
+//     divEl.append(h3El);
 
-    const pEl = document.createElement("p");
-    const bio = dog.bio;
-    pEl.innerText = bio;
-    divEl.append(pEl);
+//     const pEl = document.createElement("p");
+//     const bio = dog.bio;
+//     pEl.innerText = bio;
+//     divEl.append(pEl);
 
-    const divTwoEl = document.createElement("div")
-    divTwoEl.className = "main__dog-section__btn"
-    sectionEl.append(divTwoEl)
-const pTwoEl = document.createElement("p")
-divTwoEl.append(pTwoEl)
-const emEl = document.createElement("em")
-pTwoEl.append(emEl)
-emEl.innerText = "Is Naughty?"
-    const buttonEl = document.createElement("button")
-    divTwoEl.append(buttonEl)
-    let isGoodDog = ""
-    const dogBehaviour = dog.isGoodDog
-    if (dogBehaviour){
-      dogBehaviour = "Good Dog"
-    } else {
-      dogBehaviour = "Bad Dog"
-    }
-    const row = `${dogBehaviour}\n`
-    isGoodDog = dogBehaviour
-    buttonEl.innerText = isGoodDog
+//     const divTwoEl = document.createElement("div")
+//     divTwoEl.className = "main__dog-section__btn"
+//     sectionEl.append(divTwoEl)
+// const pTwoEl = document.createElement("p")
+// divTwoEl.append(pTwoEl)
+// const emEl = document.createElement("em")
+// pTwoEl.append(emEl)
+// emEl.innerText = "Is Naughty?"
+//     const buttonEl = document.createElement("button")
+//     divTwoEl.append(buttonEl)
+//     let isGoodDog = ""
+//     const dogBehaviour = dog.isGoodDog
+//     if (dogBehaviour){
+//       dogBehaviour = "Good Dog"
+//     } else {
+//       dogBehaviour = "Bad Dog"
+//     }
+//     const row = `${dogBehaviour}\n`
+//     isGoodDog = dogBehaviour
+//     buttonEl.innerText = isGoodDog
     
-  return sectionEl
-}
-const dogsOnlineCard = createCardElement(data[0]);
-console.log("Inside Card Online: ", dogsOnlineCard);
-for (let i = 0; i < data.length; i++) {
-    const dogs = data[i];
-    // console.log("Inside Dogs Data: ", dogs);
+//   return sectionEl
+// }
+// const dogsOnlineCard = createCardElement(data[0]);
+// console.log("Inside Card Online: ", dogsOnlineCard);
+// for (let i = 0; i < data.length; i++) {
+//     const dogs = data[i];
+//     // console.log("Inside Dogs Data: ", dogs);
 
-    const createCard = createCardElement(data)
-    sectionEl.append(createCard)
-}
+//     const createCard = createCardElement(data)
+//     sectionEl.append(createCard)
+// }
